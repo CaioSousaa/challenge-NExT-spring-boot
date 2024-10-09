8cbd99cede7012174be63646ea9763d5811fa78e
caio <caiothalles607@gmail.com>
2024-10-09 13:09:55 -0300

    feat: initial commit

---
+++ src/main/java/dev/example/NExT_challenge/NExTChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.example.NExT_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NExTChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NExTChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/example/NExT_challenge/NExTChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.example.NExT_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NExTChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
